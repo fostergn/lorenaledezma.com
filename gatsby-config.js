e3c08662b851198bad930696a955c7da7eddaf47
Gavin Foster <gavinnfoster@gmail.com>
2019-03-06 17:37:29 -0600

    updating auth and base url

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,21 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
+        baseUrl: 'https://tracheal-sod.000webhostapp.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
         protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: false,
-        auth: {},
+        auth: {
+          htaccess_user: process.env.USER,
+          htaccess_pass: process.env.PASSWORD,
+          htaccess_sendImmediately: false
+        },
         // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
+        verboseOutput: true,
+        excludedRoutes: ["**/themes"],
       },
     },
     'gatsby-plugin-sharp',
