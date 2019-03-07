ae201b7d3330e24f4b4182839a4db8b0db875bbd
Gavin Foster <gavinnfoster@gmail.com>
2019-03-06 23:30:39 -0600

    adding react player

--- src/components/ProjectSingle.js
+++ src/components/ProjectSingle.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Video from './Video'
 
-const ProjectSingle = ({ name, date, description, image, external_image_url, video }) => (
-  <div>
-    <div>{name}</div>
-    <div>{date}</div>
-  </div>
-)
+const ProjectSingle = ({ name, date, description, image, external_image_url, video }) => {
+
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{date}</div>
+      <Video url={video} />
+    </div>
+  )
+}
 
 export default ProjectSingle
---
+++ src/components/Video.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/video.css'
+import ReactPlayer from 'react-player'
+
+const Video = ({ url }) => (
+  <div className='player-wrapper'>
+    <ReactPlayer
+      className='react-player'
+      url={url}
+      width='100%'
+      height='100%'
+    />
+  </div>
+)
+
+export default Video
