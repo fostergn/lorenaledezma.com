2f88abe6e708c19287cfe419f8faba4195d195a5
Gavin Foster <gavinnfoster@gmail.com>
2019-03-06 21:11:44 -0600

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <StaticQuery
@@ -21,34 +19,20 @@ const Navbar = () => (
       <nav className="navbar is-transparent">
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              <figure className="image">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-              </figure>
-            </Link>
-          </div>
-          <div className="navbar-start">
-            {data.allWordpressPage.edges.map(edge => (
-              <Link
-                className="navbar-item"
-                to={edge.node.slug}
-                key={edge.node.slug}
-              >
-                {edge.node.title}
-              </Link>
-            ))}
+            <Link to="/" className="navbar-item">Lorena Ledezma</Link>
           </div>
           <div className="navbar-end">
-            <a
-              className="navbar-item"
-              href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
+            {data.allWordpressPage.edges
+                .filter(({ node }) => node.slug !== `homepage` )
+                .map(edge => (
+                <Link
+                  className="navbar-item"
+                  to={edge.node.slug}
+                  key={edge.node.slug}
+                >
+                  {edge.node.title}
+                </Link>
+              ))}
           </div>
         </div>
       </nav>
